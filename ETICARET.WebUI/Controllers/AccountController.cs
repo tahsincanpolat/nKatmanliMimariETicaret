b502dfb9ba0b3d7382147608c72acd2639f9bad7
tahsincanpolat <tahsincanpolat@gmail.com>
2023-04-29 12:27:10 +0300

    login, register

--- ETICARET.WebUI/Controllers/AccountController.cs
+++ ETICARET.WebUI/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using ETICARET.Business.Abstract;
+using ETICARET.WebUI.EmailService;
 using ETICARET.WebUI.Extensions;
 using ETICARET.WebUI.Identity;
 using ETICARET.WebUI.Models;
@@ -45,10 +46,22 @@ namespace ETICARET.WebUI.Controllers
 
             if (result.Succeeded)
             {
-                /* 
-                 Generate Token
-                 Email Service
-                 */
+                // generate token
+                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                var callbackUrl = Url.Action("ConfirmEmail", "Account", new
+                {
+                    userId = user.Id,
+                    token = code
+                });
+
+                // send email
+
+                string siteurl = "https://localhost:5174";
+                string activeUrl = $"{siteurl}{callbackUrl}";
+
+                string body = $"Hesabınızı onaylayınız.<br><br> Lütfen email hesabınızı onaylamak için linke <a href='{activeUrl}' target='_blank'> tıklayız</a>...";
+
+                MailHelper.SendEmail(body,model.Email,"ETICARET HESAP AKTİFLEŞTİRME");
                 return RedirectToAction("Login","Account");
             }
 
@@ -56,6 +69,51 @@ namespace ETICARET.WebUI.Controllers
 
             return View(model);
 
+        }
+
+        public async Task<IActionResult> ConfirmEmail(string userId,string token)
+        {
+            if(userId == null || token == null)
+            {
+                TempData.Put("message", new ResultMessage()
+                {
+                    Title = "Geçersiz Token",
+                    Message = "Hesap onay bilgileri yanlış",
+                    Css = "danger"
+                });
+
+                return Redirect("~/");
+            }
+
+            var user = await _userManager.FindByIdAsync(userId);
+
+            if(user != null)
+            {
+                var result = await _userManager.ConfirmEmailAsync(user, token);
+
+                if (result.Succeeded)
+                {
+                    _cartService.InitializeCart(userId);
+
+                    TempData.Put("message", new ResultMessage()
+                    {
+                        Title = "Hesap Onayı",
+                        Message = "Hesap onaylanmıştır",
+                        Css = "danger"
+                    });
+
+                    return RedirectToAction("Login","Account");
+                }
+            }
+
+            TempData.Put("message", new ResultMessage()
+            {
+                Title = "Hesap Onayı",
+                Message = "Hesap onaylanmadı",
+                Css = "danger"
+            });
+
+            return Redirect("~/");
         }
 
         public IActionResult Login(string ReturnUrl=null)
@@ -95,6 +153,94 @@ namespace ETICARET.WebUI.Controllers
             ModelState.AddModelError("", "Email veya Şifre Yanlış");
 
             return View(model);
+        }
+
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> ForgotPassword(string Email)
+        {
+            if (string.IsNullOrEmpty(Email))
+            {
+                TempData.Put("message", new ResultMessage()
+                {
+                    Title = "Forgot Password",
+                    Message = "Bilgileriniz Hatalı",
+                    Css = "danger"
+                });
+
+                return View();
+            }
+
+            var user = await _userManager.FindByEmailAsync(Email);
+
+            if(user == null)
+            {
+                TempData.Put("message", new ResultMessage()
+                {
+                    Title = "Forgot Password",
+                    Message = "Bu Email adresi ile bir kullanıcı bulunamadı",
+                    Css = "danger"
+                });
+
+                return View();
+            }
+
+            var code = await _userManager.GeneratePasswordResetTokenAsync(user);
+            var callbackUrl = Url.Action("ResetPassword", "Account", new
+            {
+                token = code
+            });
+
+            // send email
+
+            string siteurl = "https://localhost:5174";
+            string activeUrl = $"{siteurl}{callbackUrl}";
+
+            string body = $"Parolanızı yenilemek için <a href='{activeUrl}' target='_blank'> tıklayız</a>...";
+
+            MailHelper.SendEmail(body, Email, "ETICARET Şifre Yenileme");
+
+            return RedirectToAction("Login");
+        }
+
+        public IActionResult ResetPassword(string token)
+        {
+            if(token == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var model = new ResetPassword() { Token = token };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(ResetPassword model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = await _userManager.FindByEmailAsync(model.Email);
+
+            if(user == null)
+            {
+                return RedirectToAction("Index","Home");
+            }
+
+            var result = await _userManager.ResetPasswordAsync(user,model.Token,model.Password);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Login","Account");
+            }
+
+            return View("~/");
         }
 
         public async Task<IActionResult> Logout()
---
+++ ETICARET.WebUI/EmailService/MailHelper.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace ETICARET.WebUI.EmailService
+{
+    public class MailHelper
+    {
+        public static bool SendEmail(string body,string to,string subject,bool isHtml=true)
+        {
+            return SendEmail(body, new List<string> { to }, subject, isHtml);
+        }
+
+        public static bool SendEmail(string body, List<string> to, string subject, bool isHtml = true)
+        {
+            bool result = false;
+
+            try
+            {
+                var message = new MailMessage();
+                message.From = new MailAddress("test_ucuncubinyilakademi@hotmail.com");
+
+                to.ForEach(x =>
+                {
+                    message.To.Add(new MailAddress(x));
+                });
+
+                message.Subject = subject;
+                message.Body = body;
+                message.IsBodyHtml = isHtml;
+
+                using (var smtp = new SmtpClient("smtp-mail.outlook.com", 587))
+                {
+                    smtp.EnableSsl = true;
+                    smtp.Credentials = new NetworkCredential("test_ucuncubinyilakademi@hotmail.com","Uby123456");
+                    smtp.Send(message);
+                    result = true;
+                }
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return result;
+        }
+    }
+}

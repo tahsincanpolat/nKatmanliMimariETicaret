dfff6bdc41f8e059e119890dec73b521b5aa80d6
tahsincanpolat <tahsincanpolat@gmail.com>
2023-05-20 12:32:08 +0300

    comment create,delete,update

---
+++ ETICARET.WebUI/Controllers/CommentController.cs
@@ -0,0 +1,126 @@
+﻿using ETICARET.Business.Abstract;
+using ETICARET.Entities;
+using ETICARET.WebUI.Identity;
+using ETICARET.WebUI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETICARET.WebUI.Controllers
+{
+    public class CommentController : Controller
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private IProductService _productService;
+        private ICommentService _commentService;
+
+        public CommentController(UserManager<ApplicationUser> userManager, IProductService productService, ICommentService commentService)
+        {
+            _userManager = userManager;
+            _productService = productService;
+            _commentService = commentService;
+        }
+
+        public IActionResult ShowProductComments(int? id)
+        {
+            if(id == null)
+            {
+                return BadRequest();
+            }
+
+            Product product = _productService.GetProductDetails(id.Value);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return PartialView("_PartialComments", product.Comments);
+        }
+
+        [Authorize]
+        public IActionResult Delete(int? id)
+        {
+                if (id == null)
+                {
+                    return BadRequest();
+                }
+
+                Comment comment = _commentService.GetById(id.Value);
+
+                if (comment == null)
+                {
+                    return NotFound();
+                }
+
+                _commentService.Delete(comment);
+
+                return Json(new { result = true });
+
+                return Json(new { result = false });
+
+        }
+
+        [Authorize]
+        public IActionResult Edit(int? id,string text)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+
+            Comment comment = _commentService.GetById(id.Value);
+
+            if (comment == null)
+            {
+                return NotFound();
+            }
+            comment.Text = text.Trim('\n').Trim(' ');
+            comment.CreateOn = DateTime.Now;
+
+            _commentService.Update(comment);
+
+            return Json(new { result = true });
+
+            return Json(new { result = false });
+
+        }
+
+        public IActionResult Create(CommentModel model,int? productId)
+        {
+            ModelState.Remove("UserId");
+
+            if (ModelState.IsValid)
+            {
+                if(productId == null)
+                {
+                    return BadRequest();
+                }
+
+                Product product = _productService.GetById(productId.Value);
+
+                if(product == null)
+                {
+                    return NotFound();
+                }
+
+                Comment comment = new Comment()
+                {
+                    Text = model.Text.Trim('\n').Trim(' '),
+                    CreateOn = DateTime.Now,
+                    ProductId = product.Id,
+                    UserId = _userManager.GetUserId(User)
+                };
+
+                _commentService.Create(comment);
+
+                return Json(new { result = true });
+
+                return Json(new { result = false });
+
+            }
+
+            return View(model);
+        }
+    }
+}
--- ETICARET.WebUI/wwwroot/js/comment.js
+++ ETICARET.WebUI/wwwroot/js/comment.js
@@ -1,10 +1,102 @@
-﻿$(document).ready(function () {
-    var url = $("#comment").data("url");
-   
-})
+﻿CommentBodyId = "#comment";
+var productid = -1;
 
+
+$(document).ready(function () {
+    var url = $("#comment").data("url");
+    $("#comment").load(url);
+    productid = $("#comment").data("product-id");
+    $(CommentBodyId).load("/Comment/ShowProductComments?id="+productId)
+})
 
 function imageBox(smallImg) {
     var fullImg = document.getElementById("imageBox");
     fullImg.src = smallImg.src;
+}
+
+function doComment(btn, e, commentid, spanid) {
+    var button = $(btn);
+    var mode = button.data("edit-mode");
+    var editableContent = $("#comment_text_"+commentid)
+    if (e == "new_clicked") {
+        var txt = $("#new_comment_text").val();
+
+        $.ajax({
+            method: "POST",
+            url: "/Comment/Create/",
+            data: {"text":txt,"productid":productid}
+        }).done(function (data) {
+            if (data.result) {
+                $(CommentBodyId).load("/Comment/ShowProductComments?id=" + productid)
+            }
+            else {
+                alert("Yorum yapılamadı...")
+            }
+        }).fail(function (e) {
+            console.log(e);
+            alert("Sunucu ile bağlantı kurulamadı...")
+        })
+    }
+    else if (e == "delete_clicked") {
+        console.log(e);
+        var dialog_res = confirm("Yorum silinsin mi?");
+        if (!dialog_res) return false;
+
+        $.ajax({
+            method: "POST",
+            url: "/Comment/Delete?id="+commentid
+        }).done(function (data) {
+            if (data.result) {
+                $(CommentBodyId).load("/Comment/ShowProductComments?id=" + productid)
+            }
+            else {
+                alert("Yorum silinemedi...")
+            }
+        }).fail(function (e) {
+            console.log(e);
+            alert("Sunucu ile bağlantı kurulamadı...")
+        })
+    }
+    else if (e == "edit_clicked") {
+        if (!mode) {
+            button.data("edit-mode", true);
+            button.removeClass("btn-warning");
+            button.addClass("btn-success");
+            var btnSpan = button.find("span");
+            btnSpan.removeClass("fa-edit");
+            btnSpan.addClass("fa-check");
+            editableContent.addClass("editable-content");
+
+            $(spanid).attr("contenteditable", true)
+        }
+        else {
+            button.data("edit-mode", true);
+            button.removeClass("btn-succes");
+            button.addClass("btn-warning");
+            var btnSpan = button.find("span");
+            btnSpan.removeClass("fa-check");
+            btnSpan.addClass("fa-edit");
+            editableContent.removeClass("editable-content");
+
+            $(spanid).attr("contenteditable", true)
+
+            var txt = $(spanid).text();
+
+            $.ajax({
+                method: "POST",
+                url: "/Comment/Edit/",
+                data: { "text": txt, "id": commentid }
+            }).done(function (data) {
+                if (data.result) {
+                    $(CommentBodyId).load("/Comment/ShowProductComments?id=" + productid)
+                }
+                else {
+                    alert("Yorum güncellenemedi...")
+                }
+            }).fail(function (e) {
+                console.log(e);
+                alert("Sunucu ile bağlantı kurulamadı")
+            })
+        }
+    }
 }

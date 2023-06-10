6dc079eebffeda5a6406d42d2449828c74f77ca3
tahsincanpolat <tahsincanpolat@gmail.com>
2023-06-10 12:11:58 +0300

    jwt auth

--- ETICARET.JWT/Controllers/AuthController.cs
+++ ETICARET.JWT/Controllers/AuthController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace ETICARET.JWT.Controllers
@@ -15,12 +16,10 @@ namespace ETICARET.JWT.Controllers
     {
         public static User user = new User();
         private readonly IConfiguration _configuration;
-        private readonly IUserService _userService;
 
-        public AuthController(IConfiguration configuration, IUserService userService)
+        public AuthController(IConfiguration configuration)
         {
             _configuration = configuration;
-            _userService = userService;
         }
 
         [HttpGet,Authorize]
@@ -29,6 +28,37 @@ namespace ETICARET.JWT.Controllers
             var username = User?.Identity?.Name;
 
             return Ok(username);
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserDt request)
+        {
+            CreatePasswordHash(request.Password,out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.UserName = request.Username;
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<User>> Login(UserDt request)
+        {
+            if(user.UserName != request.Username)
+            {
+                return BadRequest("User Not Found");
+            }
+
+            if (!VerifyPasswordHash(request.Password,user.PasswordHash,user.PasswordSalt)) 
+            {
+                return BadRequest("Wrong Password");
+            }
+
+            string token = CreateToken(user);
+            var newRefreshToken = GenerateRefreshToken();
+            setRefreshToken(newRefreshToken);
+            return Ok(token);
         }
 
         [HttpPost("refresh-token")]
@@ -46,10 +76,37 @@ namespace ETICARET.JWT.Controllers
             }
 
             string token = CreateToken(user);
-            //var newRefreshToken 
+            var newRefreshToken = GenerateRefreshToken();
+            setRefreshToken(newRefreshToken);
             return Ok(token);
         }
 
+        private void setRefreshToken(RefreshToken newRefreshToken)
+        {
+            var cookieOptions = new CookieOptions
+            {
+                Expires = newRefreshToken.Expires,
+                HttpOnly = true
+            };
+
+            Response.Cookies.Append("refreshToken", newRefreshToken.Token, cookieOptions);
+
+            user.RefreshToken = newRefreshToken.Token;
+            user.TokenCreated = newRefreshToken.Created;
+            user.TokenExpires = newRefreshToken.Expires;
+        }
+
+        private RefreshToken GenerateRefreshToken()
+        {
+            var refreshToken = new RefreshToken()
+            {
+                Expires = DateTime.Now.AddDays(7),
+                Created = DateTime.Now,
+                Token = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64))
+            };
+
+            return refreshToken;
+        }
 
         private string CreateToken(User user)
         {
@@ -71,6 +128,24 @@ namespace ETICARET.JWT.Controllers
             var jwt = new JwtSecurityTokenHandler().WriteToken(token);
 
             return jwt;
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash,byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA256(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
+        }
+
+        private void CreatePasswordHash(string password,out byte[] passwordHash,out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA256())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
         }
     }
 }
--- ETICARET.JWT/Program.cs
+++ ETICARET.JWT/Program.cs
@@ -1,3 +1,4 @@
+using ETICARET.JWT.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
@@ -11,6 +12,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddHttpContextAccessor();
+
 builder.Services.AddSwaggerGen(options =>
 {
     options.AddSecurityDefinition("oauth", new OpenApiSecurityScheme
